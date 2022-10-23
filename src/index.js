ce066a1c517da7f8c1bf7671b12409248e483360
Carlos Thompson <ce-thompson@criticalsoftware.com>
2022-10-23 18:05:21 +0100

    created checkout page along with checkout items and checkout cart functionality

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
 import { UserProvider } from "./contexts/user.context";
-import { ProductsProvider } from "./contexts/product.context";
+import { ProductsProvider } from "./contexts/products.context";
 import { CartProvider } from "./contexts/cart.context";
 
 import "./index.scss";
