47ba938495a86e5a04ad592abf0a137c5691a772
Carlos Thompson <ce-thompson@criticalsoftware.com>
2022-10-31 11:55:49 +0000

    fix shop component and styled-component and adding redirect for netlify

--- src/components/directory-item/directory-item.styles.jsx
+++ src/components/directory-item/directory-item.styles.jsx
@@ -5,8 +5,8 @@ export const BackgroundImage = styled.div`
   height: 100%;
   background-size: cover;
   background-position: center;
-  background-image: ${({imageUrl}) => `url(${imageUrl})`};
-`
+  background-image: ${({ imageUrl }) => `url(${imageUrl})`};
+`;
 
 export const Body = styled.div`
   height: 90px;
@@ -32,37 +32,37 @@ export const Body = styled.div`
     font-weight: lighter;
     font-size: 16px;
   }
-`
+`;
 
 export const DirectoryItemContainer = styled.div`
   min-width: 30%;
-    height: 240px;
-    flex: 1 1 auto;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border: 1px solid black;
-    margin: 0 7.5px 15px;
-    overflow: hidden;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
 
-    &:hover {
-      cursor: pointer;
+  &:first-child {
+    margin-right: 7.5px;
+  }
 
-      & ${BackgroundImage} {
-        transform: scale(1.1);
-        transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-      }
+  &:last-child {
+    margin-left: 7.5px;
+  }
 
-      & ${Body} {
-        opacity: 0.9;
-      }
+  &:hover {
+    cursor: pointer;
+
+    ${BackgroundImage} {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
     }
 
-    &:first-child {
-      margin-right: 7.5px;
+    ${Body} {
+      opacity: 0.9;
     }
-
-    &:last-child {
-      margin-left: 7.5px;
-    }
-`
+  }
+`;
